a266a1007e73d3c78a9c89945ddf486577a06715
DINKAL YADUWANSHI <ansh24275@gmail.com>
2026-09-07 09:41:14 +0530

    Time: 3 ms (31.24%) | Memory: 11.9 MB (18.07%) - LeetSync

---
+++ 412-fizz-buzz/fizz-buzz.cpp
@@ -0,0 +1,27 @@
+class Solution {
+public:
+    vector<string> fizzBuzz(int n) {
+        vector<string> ans;
+        for(int i = 1; i<=n;i++){
+            string s = "";
+            if(i%3 == 0 && i%5 == 0){
+                s += "FizzBuzz";
+                ans.push_back(s);
+            }
+
+            else if(i%5 == 0){
+                s += "Buzz";
+                ans.push_back(s);
+            }
+            else if(i%3 == 0){
+                s += "Fizz";
+                ans.push_back(s);
+            }
+            else{
+                s += to_string(i);
+                ans.push_back(s);
+            }
+        }
+        return ans;
+    }
+};
