dd8268ee2be56903c5cbb7bc9b8297c8653a2d4b
DINKAL YADUWANSHI <ansh24275@gmail.com>
2026-09-15 22:52:34 +0530

    Time: 11 ms (8.62%) | Memory: 263.5 MB (18.61%) - LeetSync

--- 189-rotate-array/rotate-array.cpp
+++ 189-rotate-array/rotate-array.cpp
@@ -4,15 +4,9 @@ public:
         int n = nums.size();
         k = k%n;
         if (k == 0) return;
-        vector<int> temp(k);
-        for(int i = 0; i<k; i++){
-            temp[i]=nums[n-k+i];
-        }
-        for(int i = n - k - 1; i >= 0; i--){
-            nums[i+k]=nums[i];
-        }
-        for(int i = 0; i<k;i++){
-            nums[i]=temp[i];
-        }
+         
+        reverse(nums.begin(), nums.end());
+        reverse(nums.begin(), nums.begin() + k);
+        reverse(nums.begin() + k, nums.end());
     }
 };
